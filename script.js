b360d41cad1c8d0fa504936d4a1cd12d545b550a
rose-kg <rosekgatle99@gmail.com>
2024-05-22 14:35:35 +0200

    made more changes

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navLinks.forEach(link => link.classList.remove('active'));
+            link.classList.add('active');
+        });
+    });
+});
