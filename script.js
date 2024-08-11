2326e56a7adea4379f3911a3f0f0f91df74e2eac
rose-kg <rosekgatle99@gmail.com>
2024-08-11 23:19:39 +0200

    made website responsive for all screens

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuButton = document.querySelector('.dropdown-toggle');
     const dropdownMenu = document.querySelector('.dropdown-menu');
+    const menuItems = dropdownMenu.querySelectorAll('a'); 
 
     if (menuButton && dropdownMenu) {
         menuButton.addEventListener('click', () => {
             const isVisible = dropdownMenu.style.display === 'block';
             dropdownMenu.style.display = isVisible ? 'none' : 'block';
         });
+
+        menuItems.forEach(item => {
+            item.addEventListener('click', () => {
+                dropdownMenu.style.display = 'none';
+            });
+        });
     }
 });
-
-
